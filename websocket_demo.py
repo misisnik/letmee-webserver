a757286b752f23980044075c7e570bdca6942081
Michal Sladecek <misisnik@gmail.com>
2018-04-19 11:02:25 +0200

    Added wifi configuration, prepared urat and predefined led map

--- websocket_demo.py
+++ websocket_demo.py
@@ -1,6 +1,8 @@
 import time
-# import machine
+from machine import UART
+import network
 import ujson
+import ubinascii
 from ws_connection import ClientClosedError
 from ws_server import WebSocketServer, WebSocketClient
 
@@ -61,31 +63,77 @@ class Server(WebSocketServer):
 class Letmee(object):
     def __init__(self):
         # initialize of attiny UART communication
+        self.config = self.loadConfig()
+        self.setNetwork()
         self.attiny = self.attinyInit()
 
+    def loadConfig(self):
+        """
+            Load json data from config file with all settings
+        """
+        with open('config', 'r') as f:
+            config = f.readline()
+        return ujson.loads(config)
+
+    def saveConfig(self, config = None):
+        """
+            Save config file from self.config if is not defined
+        """
+        # change config file
+        with open('config', 'w') as f:
+            if config == None:
+                config = self.config
+            f.write(ujson.dumps(config))
+        self.config = config
+
+    def getMac(self):
+        """
+            Return mac address of the tag
+        """
+        return ubinascii.hexlify(network.WLAN().config('mac'),':').decode()
+
+    def setNetwork(self):
+        name = '{}-{}'.format(self.config['name'], self.getMac()[-4:])
+        ap_if = network.WLAN(network.AP_IF)
+        ap_if.active(True)
+        ap_if.config( essid    = name
+                    , password = self.config['pass'] )
+
+        print('nastaveno')
+
     def attinyInit(self):
-        pass
+        # define UART
+        self.uart = UART(1, 9600)
+        self.uart.init(9600, bits=8, parity=None, stop=1)
 
     def checkUart(self):
         # todo UART read something if exist and set timeout on it because i want !
         # _NEW_DATA = {'t':0, 'e': 0, 'p': 0}
-        return {'t':0, 'e': _NEW_DATA['e'] + 1, 'p': _NEW_DATA['p'] + 1}
+        # return {'t':0, 'e': _NEW_DATA['e'] + 1, 'p': _NEW_DATA['p'] + 1}
 
-print('start')
-server = Server()
+        # readline with timeout
+        try:
+            line = self.uart.readline()
+            return ujson.loads(line)
+        except Exception as e:
+            print(e)
+            return False
+
 letmee = Letmee()
+server = Server()
 server.start()
+print('server_started')
 try:
     while True:
         server.process_all()
-        server.process_all()
         # set uart event flah to 0 and new_data erase
         _UART_FLAG = False
-        # _NEW_DATA = {}
         # something on UART??
         if _INIT_FLAG:
-            _NEW_DATA = letmee.checkUart()
-            _UART_FLAG = True
+            uart_data = letmee.checkUart()
+            if uart_data:
+                _NEW_DATA = uart_data
+                _UART_FLAG = True
         time.sleep(1)
 except Exception as e:
     print('foooooo')
